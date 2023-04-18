f4f0ef33ea96a60d2b0549041ff3488c4464150e
alexpaynter <alexcpaynter@gmail.com>
2023-04-18 00:36:55 +0000

    Updates, quick report

---
+++ analysis/scripts/upload_outputs_synapse.R
@@ -0,0 +1,8 @@
+output_brca_synid <- "syn51359343" #2023-04-17-BrCa-landscape-paper-outputs
+
+library(synapser)
+
+synLogin()
+synapser::File(here("analysis", "reports", "brca_regimens.html"),
+               parent = output_brca_synid) %>%
+  synStore()
