39cac16520863c9dc38dcaeb440c273433af0ce2
alexpaynter <alexcpaynter@gmail.com>
2023-06-01 15:54:51 -0700

    Added cpt test gathering

---
+++ R/get_cpt_by_time.R
@@ -0,0 +1,50 @@
+
+#' @title Get cancer panel test by time
+#' @description Get all cancer panel tests BEFORE the time given in 
+#'   time_dat$time_var, or any tests which are the first cancer panel test. 
+#'   Filtering is also limited only to {record_id, ca_seq} pairs in
+#'   time_dat.
+#' @details 
+get_cpt_by_time <- function(
+    time_dat,
+    time_var,
+    cpt_dat,
+    time_tol = 0,
+    always_keep_first = T) {
+  if (!all(c('record_id', 'ca_seq', time_var) %in% names(time_dat))) {
+    cli::cli_abort("time_dat must have columns {record_id, ca_seq} and time_var.")
+  }
+  if (any(time_dat[[time_var]] > 100)) {
+    cli::cli_abort("time_var is assumed to be in years, but values over 100 were detected.  Wrong units?")
+  }
+  
+  time_dat <- time_dat %>%
+    select(record_id, ca_seq, all_of(time_var))
+  time_dat_max <- time_dat %>% count(record_id, ca_seq, sort = T) %>% pull %>% max
+  if (time_dat_max > 1) {
+    cli_abort("More than one record per {record_id, ca_seq} in time_dat - these should be unique!")
+  }
+  
+  rtn_dat <- inner_join(
+    cpt_dat,
+    time_dat,
+    by = c("record_id", "ca_seq")
+  )
+  
+  rtn_dat %<>%
+    group_by(record_id, ca_seq) %>%
+    arrange(dx_cpt_rep_yrs) %>%
+    mutate(
+      is_first_cpt = row_number() %in% 1,
+      cpt_before_t = dx_cpt_rep_yrs <= .data[[time_var]] + time_tol
+    ) %>%
+    ungroup(.) 
+  
+  if (always_keep_first) {
+    rtn_dat %<>% filter(is_first_cpt | cpt_before_t)
+  } else {
+    rtn_dat %<>% filter(cpt_before_t)
+  }
+  
+  return(rtn_dat)
+}
--- analysis/explore/gene_explore.R
+++ analysis/explore/gene_explore.R
@@ -1,8 +1,13 @@
 library(magrittr)
 library(dplyr)
 library(janitor)
+library(yaml)
+library(here)
 
-dft_
+purrr::walk(.x = fs::dir_ls('R'), .f = source)
+
+dft_cpt <- readr::read_csv(here('data-raw', 'cancer_panel_test_level_dataset.csv'))
+glimpse(dft_cpt)
 
 dft_cna <- readr::read_tsv(here('data-raw', 'data_CNA.txt'))
 dft_cna %>% glimpse
@@ -22,6 +27,27 @@ dft_cpt <- readr::read_csv(
 dft_gene_mat <- readr::read_tsv(here('data-raw', 'data_gene_matrix.txt'))
 dft_gene_mat %>% glimpse
 
-dft_gp_test <- readLines(
+dft_gp_test <- read_yaml(
   here('data-raw', 'data_gene_panel_DFCI-ONCOPANEL-1.txt')
-) 
+)
+
+
+
+
+
+
+# Load some data to play with:
+dft_pt <- readr::read_csv(here('data-raw', 'patient_level_dataset.csv'))
+dft_ca_ind <- readr::read_csv(here('data-raw', 'cancer_level_dataset_index.csv'))
+dft_dmet_timing <- get_dmet_timing(ca_ind_df = dft_ca_ind)
+
+
+# Example:  Get all NGS which occur prior to metastatic cancer, or any which are
+#   the first NGS test.
+get_cpt_by_time(
+  time_dat = dft_dmet_timing,
+  time_var = "tt_y",
+  cpt_dat = dft_cpt,
+  always_keep_first = T
+)
+
