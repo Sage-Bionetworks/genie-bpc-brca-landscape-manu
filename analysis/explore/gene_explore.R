d7989a1ba75f45e5b484d9ec4947358f75ea1968
alexpaynter <alexcpaynter@gmail.com>
2023-06-01 18:17:00 -0700

    additional exploration on genes

--- analysis/explore/gene_explore.R
+++ analysis/explore/gene_explore.R
@@ -3,6 +3,9 @@ library(dplyr)
 library(janitor)
 library(yaml)
 library(here)
+library(purrr)
+library(fs)
+library(ggplot2)
 
 purrr::walk(.x = fs::dir_ls('R'), .f = source)
 
@@ -14,6 +17,12 @@ dft_cna %>% glimpse
 
 dft_mut <- readr::read_tsv(here('data-raw', 'data_mutations_extended.txt'))
 dft_mut %>% glimpse
+# some things which may help us filter a bit:
+dft_mut$Consequence %>% unique
+dft_mut$Variant_Classification %>% unique
+dft_mut$Polyphen_Prediction %>% unique 
+# Polyphen Score doesn't seem useful in addition due to...
+ggplot(dft_mut, aes(x = Polyphen_Prediction, y = Polyphen_Score)) + geom_jitter()
 dft_mut %>% tabyl(Hugo_Symbol) %>% arrange(desc(n)) %>% head(10)
 # Probably start here.
 dft_mut %>% glimpse
@@ -44,6 +53,7 @@ tidy_gene_panel(
 dft_pt <- readr::read_csv(here('data-raw', 'patient_level_dataset.csv'))
 dft_ca_ind <- readr::read_csv(here('data-raw', 'cancer_level_dataset_index.csv'))
 dft_dmet_timing <- get_dmet_timing(ca_ind_df = dft_ca_ind)
+dft_gp_all <- readr::read_rds(here('data', 'gene_panel_all.rds'))
 
 
 # Example:  Get all NGS which occur prior to metastatic cancer, or any which are
