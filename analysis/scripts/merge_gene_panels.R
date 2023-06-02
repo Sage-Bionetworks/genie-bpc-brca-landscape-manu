3153615d45a7499729a6145da461c7f23d08ced2
alexpaynter <alexcpaynter@gmail.com>
2023-06-01 17:57:07 -0700

    Added dervied data for all gene panels

--- analysis/scripts/merge_gene_panels.R
+++ analysis/scripts/merge_gene_panels.R
@@ -3,12 +3,25 @@ library(purrr)
 library(fs)
 library(tibble)
 library(dplyr)
+library(yaml)
 
 purrr::walk(fs::dir_ls('R'), .f = source)
 
-dft_gene_panels <- fs::dir_ls('data-raw') %>%
-  str_filter(., 'data_gene_panel_.*') %>%
-  tibble(path = .)
+vec_gene_panels <- fs::dir_ls('data-raw') %>%
+  str_filter(., 'data_gene_panel_.*')
 
-dft_gene_panels %>% slice(1) %>% pull(path) %>% tidy_gene_panel(.)
+gp_all <- purrr::map_dfr(
+  .x = vec_gene_panels,
+  .f = tidy_gene_panel
+)
 
+# gp_all_wide <- gp_all %>%
+#   tidyr::pivot_wider(
+#     names_from = gene,
+#     values_from = tested
+#   )
+
+saveRDS(
+  object = gp_all,
+  file = here('data', 'gene_panel_all.rds')
+)
