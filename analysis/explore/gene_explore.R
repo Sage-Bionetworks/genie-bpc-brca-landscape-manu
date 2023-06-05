7d76a6368839d73e305606a30da9b459a879f0da
alexpaynter <alexcpaynter@gmail.com>
2023-06-05 16:37:51 -0700

    Covariate merging exploration figured out

--- analysis/explore/gene_explore.R
+++ analysis/explore/gene_explore.R
@@ -6,63 +6,35 @@ library(here)
 library(purrr)
 library(fs)
 library(ggplot2)
+library(tidyr)
+library(survival)
+library(glmnet)
 
 purrr::walk(.x = fs::dir_ls('R'), .f = source)
 
+
+# Load some data to play with:
+dft_pt <- readr::read_rds(
+  here('data', 'clin_data_cohort', 'dft_pt.rds')
+)
+dft_ca_ind <- readr::read_rds(
+  here('data', 'clin_data_cohort', 'dft_ca_ind.rds')
+)
 dft_cpt <- readr::read_rds(
   here('data', 'clin_data_cohort', 'dft_cpt.rds')
 )
-glimpse(dft_cpt)
 
-dft_cna <- readr::read_tsv(here('data-raw', 'data_CNA.txt'))
-dft_cna %>% glimpse
-
-dft_mut <- readr::read_tsv(here('data-raw', 'data_mutations_extended.txt'))
-dft_mut %>% glimpse
-# some things which may help us filter a bit:
-dft_mut$Consequence %>% unique
-dft_mut$Variant_Classification %>% unique
-dft_mut$Polyphen_Prediction %>% unique 
-dft_mut$SIFT_Prediction %>% unique 
-# Polyphen Score doesn't seem useful in addition due to...
-# gplot(dft_mut, aes(x = Polyphen_Prediction, y = Polyphen_Score)) + geom_jitter()
-
-
-dft_mut %>% lapply(X = ., FUN = (function(x) mean(is.na(x))))
-# Notes on missingness:  Consequence is more complete than polyphen score.
-
-dft_mut %>% tabyl(Hugo_Symbol) %>% arrange(desc(n)) %>% head(10)
-# Probably start here.
-dft_mut %>% glimpse
-
-dft_fusion <- readr::read_tsv(here('data-raw', 'data_fusions.txt'))
-
-dft_cpt <- readr::read_csv(
-  here('data-raw', 'cancer_panel_test_level_dataset.csv')
+dft_gp_all <- readr::read_rds(
+  here('data', 'gene_panel_all.rds')
 )
-
-dft_gene_mat <- readr::read_tsv(here('data-raw', 'data_gene_matrix.txt'))
-dft_gene_mat %>% glimpse
-
-dft_gp_test <- read_yaml(
-  here('data-raw', 'data_gene_panel_DFCI-ONCOPANEL-1.txt')
-)
-
-
-tidy_gene_panel(
-  here('data-raw', 'data_gene_panel_DFCI-ONCOPANEL-1.txt')
+dft_gene_feat <- readr::read_rds(
+  here('data', 'gene_feat_long.rds')
 )
 
 
 
 
-
-# Load some data to play with:
-dft_pt <- readr::read_csv(here('data-raw', 'patient_level_dataset.csv'))
-dft_ca_ind <- readr::read_csv(here('data-raw', 'cancer_level_dataset_index.csv'))
 dft_dmet_timing <- get_dmet_timing(ca_ind_df = dft_ca_ind)
-dft_gp_all <- readr::read_rds(here('data', 'gene_panel_all.rds'))
-
 
 # Example:  Get all NGS which occur prior to metastatic cancer, or any which are
 #   the first NGS test.
@@ -73,5 +45,125 @@ dft_cpt_dmet <- get_cpt_by_time(
   always_keep_first = T
 )
 
-dft_cpt_dmet %>% glimpse
-dft_cpt_dmet %>% tabyl(cpt_seq_assay_id)
+dft_cpt_dmet %<>%
+  select(
+    record_id, 
+    ca_seq, 
+    cpt_genie_sample_id,
+    dx_cpt_rep_yrs,
+    is_first_cpt,
+    cpt_before_t,
+    cpt_seq_assay_id
+  )
+
+dft_gene_feat_dmet <- dft_gene_feat %>%
+  filter(cpt_genie_sample_id %in% unique(dft_cpt_dmet$cpt_genie_sample_id))
+
+# The missingness and variance criteria build on McGough et. al. (2021), section
+#. 4 titled "real-world data application"
+# Further filter the genes down to those which were tested in at least 70% of panels.
+dft_gene_feat_dmet %<>%
+  group_by(hugo) %>%
+  mutate(hugo_prop_tested = mean(tested %in% T)) %>%
+  ungroup(.) %>%
+  filter(hugo_prop_tested > 0.7)
+
+# Also require that there is some variance in the gene test results.
+dft_gene_feat_dmet %<>%   
+  group_by(hugo) %>%
+  mutate(hugo_var = var(variant, na.rm = T)) %>%
+  ungroup(.) %>%
+  filter(hugo_var > 0)
+
+dft_gene_feat_dmet %<>% 
+  select(
+    -c(
+      tested, 
+      hugo_prop_tested, 
+      hugo_var,
+      cpt_seq_assay_id
+    )
+  ) 
+
+dft_gene_feat_dmet %<>%
+  pivot_wider(
+    names_from = "hugo",
+    values_from = "variant",
+    values_fill = NA
+  )
+# Should be all 1's:
+# count(dft_gene_feat_dmet, cpt_genie_sample_id, sort = T)
+# count(dft_cpt_dmet, cpt_genie_sample_id, sort = T)
+
+dft_gene_feat_dmet <- left_join(
+  dft_cpt_dmet,
+  dft_gene_feat_dmet,
+  by = c("cpt_genie_sample_id")
+)
+
+# Now we have to deal with people that have more than one test:
+# count(dft_gene_feat_dmet, record_id, ca_seq, sort = T)
+# OK, we have two people, so it doesn't seem too important here.  
+# Let's drop their second tests.
+dft_gene_feat_dmet %<>%
+  filter(is_first_cpt) 
+# Problem solved - verify with most recent count command.
+
+cli::cli_alert_danger(text = "Need to do imputation still!!!!")
+
+
+
+# Could add other covariates here from dft_pt, but just to start:
+dft_bl <- dft_pt %>%
+  mutate(
+    white = case_when(
+      is.na(naaccr_ethnicity_code) ~ NA_real_,
+      naaccr_race_code_primary %in% "White" ~ 1,
+      T ~ 0
+    ),
+    hispanic = case_when(
+      is.na(naaccr_ethnicity_code) ~ NA_real_,
+      naaccr_ethnicity_code %in% "Non-Spanish; non-Hispanic" ~ 0,
+      T ~ 1
+    )
+  ) %>%
+  select(record_id, white, hispanic)
+      
+
+dft_bl <- dft_ca_ind %>%
+  select(
+    record_id, 
+    ca_seq, # works because we've already selected one row per person.
+    age_dx, 
+    stage_dx_iv, 
+    os_dx_status, 
+    tt_os_dx_yrs
+  ) %>%
+  full_join(., dft_dmet_bl, by = "record_id") 
+
+dft_dmet_surv <- left_join(
+  dft_gene_feat_dmet,
+  dft_bl,
+  by = c("record_id", "ca_seq"),
+  multiple = "error"
+)
+
+# Limitation of the method:
+dft_dmet_surv %<>%
+  filter(!(dx_cpt_rep_yrs > tt_os_dx_yrs))
+
+y_dmet <- with(
+  dft_dmet_surv,
+  Surv(
+    time = dx_cpt_rep_yrs,
+    time2 = tt_os_dx_yrs,
+    event = os_dx_status
+  )
+)
+
+x_dmet %>% 
+  select(
+  
+
+
+    
