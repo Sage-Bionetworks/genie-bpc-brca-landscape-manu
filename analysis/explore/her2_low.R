f6bfa560a3e15ee03a3855f21a55dba596cc8b73
alexpaynter <alexcpaynter@gmail.com>
2025-02-04 16:06:25 -0800

    her2low script added - totally ancillary

---
+++ analysis/explore/her2_low.R
@@ -0,0 +1,119 @@
+# This uses a criterion for HER2-low passed on by Jen Hoppe.
+# HER2 low is people who meet either of the following criteria:
+#  1. Have an IHC of 1+ 
+#  2. Have an IHC of 2+ AND ISH negative.
+# This seems to match the trastuzumab deruxtecan label, but may not be appropriate
+#   for other uses.
+
+# We will take the IHC values from the case-level summmaries in the cancer
+#   index dataset.  For the ISH values we summarize each person in the pathology
+#   records, marking anyone who has at least one negative ISH test.  A more
+#   strict criterion would be requiring that there are no positive ISH tests.
+#   To use the strict criterion, just toggle the following flag to TRUE:
+use_strict_filtering <- TRUE 
+
+library(tidyverse)
+library(readr)
+library(here)
+
+# Update data paths as needed for your system.
+dft_ca_ind <- readr::read_csv(
+  here('data-raw', 'cancer_level_dataset_index.csv')
+)
+dft_path <- readr::read_csv(
+  here('data-raw', 'pathology_report_level_dataset.csv')
+)
+
+dft_her2_low <- dft_ca_ind %>%
+  select(record_id, ca_seq, ca_bca_her2ihc_val)
+
+dft_ish_summary <- dft_path %>% 
+  select(record_id, path_proc_number, path_rep_number,
+         matches("^path_her[2]*ish_[1-5]")) %>%
+  pivot_longer(
+    cols = matches("^path_her[2]*ish_[1-5]")
+  )
+
+neg_ihc_vec <- c("Negative")
+if (use_strict_filtering) {
+  dft_ish_summary <- dft_ish_summary %>%
+      group_by(record_id) %>%
+      summarize(ish_neg = any(value %in% neg_ihc_vec) & all(!value %in% "Positive"))
+} else {
+  dft_ish_summary <- dft_ish_summary %>%
+    group_by(record_id) %>%
+    summarize(ish_neg = any(value %in% neg_ihc_vec))
+}
+
+dft_her2_low <- left_join(
+  dft_her2_low, 
+  dft_ish_summary,
+  by = c("record_id")
+)
+
+dft_her2_low <- dft_her2_low %>%
+  mutate(
+    ihc_1 = ca_bca_her2ihc_val %in% "Score of 1+",
+    ihc_2_ish_neg = ca_bca_her2ihc_val %in% "Score of 2+" & ish_neg,
+    her2_low = ihc_1 | ihc_2_ish_neg
+  )
+
+# Adding POSSIBLE HER2 ultra-low - which is a subcategory of IHC = 0 and HER2-.
+dft_her2_low <- dft_her2_low %>%
+  mutate(
+    her2_ultra_low_possible = ca_bca_her2ihc_val %in% "Score 0" & ish_neg
+  )
+
+# Counts of CASES (more than one per person possible):
+dft_her2_low %>% summarize(across(ihc_1:her2_low, .fns = sum))
+
+# Counts of PEOPLE (ignoring case order)
+dft_her2_low %>% 
+  arrange(desc(her2_low)) %>%
+  group_by(record_id) %>%
+  slice(1) %>% # one row per person
+  ungroup %>%
+  summarize(across(ihc_1:her2_low, .fns = sum))
+
+# Counts of PEOPLE who only had ONE cancer (a breast cancer):
+dft_her2_low %>% 
+  filter(ca_seq %in% 0) %>%
+  summarize(across(ihc_1:her2_low, .fns = sum))
+
+
+
+
+
+
+library(purrr); library(fs); library(here);
+purrr::walk(.x = fs::dir_ls('R'), .f = source)
+
+# Now doing the dmet time first:
+count(dft_ca_ind, record_id) %>% nrow # number of people in this cohort.
+get_dmet_timing(dft_ca_ind) %>% nrow # number of people with met dx anytime
+dft_her2_low %>%
+  group_by(record_id) %>%
+  arrange(desc(ish_neg)) %>%
+  slice(1) %>%
+  select(record_id, ish_neg) %>%
+  left_join(
+    get_dmet_timing(dft_ca_ind),
+    ., 
+    by = 'record_id'
+  ) %>%
+  count(ish_neg)
+
+dft_her2_low %>%
+  # take their her2 low cancer if they have more than one:
+  group_by(record_id) %>%
+  arrange(desc(her2_ultra_low_possible)) %>%
+  slice(1) %>%
+  select(record_id, her2_ultra_low_possible) %>%
+  left_join(
+    get_dmet_timing(dft_ca_ind),
+    ., 
+    by = 'record_id'
+  ) %>%
+  count(her2_ultra_low_possible)
+            
+
