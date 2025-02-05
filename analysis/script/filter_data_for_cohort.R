3839bbaccbcc755174dafbd9caeb5c3bab8bbb92
alexpaynter <alexcpaynter@gmail.com>
2025-02-04 16:42:38 -0800

    works through filter_data_for_cohort.R

--- analysis/script/filter_data_for_cohort.R
+++ analysis/script/filter_data_for_cohort.R
@@ -1,12 +1,8 @@
 # Description:  Filters the data to only index cancer rows.  Also removes 
 #  participants known to have Sarcoma, a cohort criterion agreed upon by group.
 
-library(dplyr)
-library(purrr)
-library(readr)
-library(magrittr)
-
 # Load all helper functions
+library(purrr); library(here); library(fs);
 purrr::walk(.x = fs::dir_ls('R'), .f = source)
 
 dft_pt <- readr::read_csv(here('data-raw', 'patient_level_dataset.csv'))
@@ -19,11 +15,7 @@ dft_reg <- readr::read_csv(here('data-raw', 'regimen_cancer_level_dataset.csv'))
 dft_tm <- readr::read_csv(here('data-raw', 'tm_level_dataset.csv'))
 
 # Not touching some datasets including:
-# - cancer_level_dataset_non_index.csv
-# - all genetic datasets and gene panel level datasets (processed separately)
 
-# Note:  ca_hist_adeno_squamous is about 20% missing, so this criterion has
-# limited appeal.
 vec_not_sarcoma <- dft_ca_ind %>%
   select(record_id, ca_seq, ca_hist_adeno_squamous) %>%
   filter(!(ca_hist_adeno_squamous %in% "Sarcoma")) %>%
